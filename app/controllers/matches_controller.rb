e3954dcf021523fde2f74328eb9e209ed2ae4487
giozerias <giobjuarez@gmail.com>
2014-05-19 17:00:53 -0400

    Started coding logic for matches and started to build the view

---
+++ app/controllers/matches_controller.rb
@@ -0,0 +1,91 @@
+class MatchesController < ApplicationController
+  before_action :set_match, only: [:show, :edit, :update, :destroy]
+
+  # GET /matches
+  # GET /matches.json
+  def index
+    @matches = Match.all
+  end
+
+  # GET /matches/1
+  # GET /matches/1.json
+  def show
+  end
+
+  # GET /matches/new
+  def new
+    @match = Match.new
+  end
+
+  # GET /matches/1/edit
+  def edit
+  end
+
+  def roll_the_dice
+    dice = [1, 2, 3, 4, 5, 6]
+    dice.shuffle.sample
+  end
+
+  # POST /matches
+  # POST /matches.json
+  def create
+    @match = Match.new(match_params)
+  
+    hero_a_superscore = Superhero.find(match_params[:hero_a_id]).superscore.to_f 
+    hero_b_superscore = Superhero.find(match_params[:hero_b_id]).superscore.to_f
+
+    hero_a_dice_result = roll_the_dice
+    hero_b_dice_result = roll_the_dice
+
+    hero_a_score = hero_a_dice_result * hero_a_superscore
+    hero_b_score = hero_b_dice_result * hero_b_superscore
+
+    @match[:hero_a_score] = hero_a_score
+    @match[:hero_b_score] = hero_b_score
+
+    respond_to do |format|
+      if @match.save
+        format.html { redirect_to @match, notice: 'Match was successfully created.' }
+        format.json { render action: 'show', status: :created, location: @match }
+      else
+        format.html { render action: 'new' }
+        format.json { render json: @match.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /matches/1
+  # PATCH/PUT /matches/1.json
+  def update
+    respond_to do |format|
+      if @match.update(match_params)
+        format.html { redirect_to @match, notice: 'Match was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: 'edit' }
+        format.json { render json: @match.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /matches/1
+  # DELETE /matches/1.json
+  def destroy
+    @match.destroy
+    respond_to do |format|
+      format.html { redirect_to matches_url }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_match
+      @match = Match.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def match_params
+      params.require(:match).permit(:hero_a_id, :hero_b_id, :hero_a_score, :hero_b_score)
+    end
+end
