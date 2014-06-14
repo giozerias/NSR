5bd6e446397b6f9f73ea7d6e5eda12588bea2e54
Giovanna Juarez <giobjuarez@gmail.com>
2014-06-14 15:22:43 -0400

    Add method to calculate score

--- app/models/superhero.rb
+++ app/models/superhero.rb
@@ -6,4 +6,8 @@ class Superhero < ActiveRecord::Base
     dice = [1, 2, 3, 4, 5, 6]
     dice.shuffle.sample
   end
+
+  def total_score(hero=self)
+		roll_the_dice * hero.superscore.to_f
+  end
 end
--- spec/models/superhero_spec.rb
+++ spec/models/superhero_spec.rb
@@ -1,8 +1,10 @@
 require 'spec_helper'
 
 describe Superhero do
-	let(:superhero) { Superhero.new(name: "spiderman", city: "atl", superpower: "spiderweb", superscore: 0.2) }
-
+	let(:superhero) { Superhero.new(name: "spiderman", city: "atl",
+																	superpower: "spiderweb", superscore: 0.2) }
+	let(:challenger) { Superhero.new(name: "batman", city: "gothem city",
+																	 superpower: "batmobile", superscore: 0.4) }
 	describe "attributes" do
 		it "has a name" do
 			expect(superhero.name).to eq("spiderman")
@@ -28,4 +30,15 @@ describe Superhero do
 	  end
 	end
 
+	describe "total score" do
+		it "returns the total score" do
+			score = superhero.total_score
+			expect(score).to be_between(1*superhero.superscore, 6*superhero.superscore)
+		end
+
+		it "returns another score for hero provided" do
+			score = superhero.total_score(challenger)
+			expect(score).to be_between(1*challenger.superscore, 6*challenger.superscore)
+		end
+	end
 end
