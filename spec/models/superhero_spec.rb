40ee0e093f9d6dc51ec1a8c47ee9b3bf64eeb77d
Giovanna Juarez <giobjuarez@gmail.com>
2014-06-14 14:29:11 -0400

    Refactored specs

--- spec/models/superhero_spec.rb
+++ spec/models/superhero_spec.rb
@@ -1,9 +1,9 @@
 require 'spec_helper'
 
 describe Superhero do
-	describe "attributes" do
-		let(:superhero) { Superhero.new(name: "spiderman", city: "atl", superpower: "spiderweb", superscore: 0.2) }
+	let(:superhero) { Superhero.new(name: "spiderman", city: "atl", superpower: "spiderweb", superscore: 0.2) }
 
+	describe "attributes" do
 		it "has a name" do
 			expect(superhero.name).to eq("spiderman")
 		end
@@ -21,10 +21,8 @@ describe Superhero do
 		end
 	end
 
-
 	describe "roll the dice method" do
 	  it "returns a number between 1 and 6" do
-	  	superhero = Superhero.new
 	  	a_number = superhero.roll_the_dice
 	  	expect(a_number).to be_between(1,6)
 	  end
