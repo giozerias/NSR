fcf8d57273e61f2f9df02f88ff51f4089ed29cf6
Giovanna Juarez <giobjuarez@gmail.com>
2014-06-14 13:04:34 -0400

    Moved roll_the_dice method to superhero model

--- app/models/superhero.rb
+++ app/models/superhero.rb
@@ -1,4 +1,9 @@
 class Superhero < ActiveRecord::Base
 	belongs_to :match
-	#to do: change this to has_many relationship 
+	#to do: change this to has_many relationship
+
+	def roll_the_dice
+    dice = [1, 2, 3, 4, 5, 6]
+    dice.shuffle.sample
+  end
 end
--- spec/models/superhero_spec.rb
+++ spec/models/superhero_spec.rb
@@ -1,5 +1,11 @@
 require 'spec_helper'
 
 describe Superhero do
-  pending "add some examples to (or delete) #{__FILE__}"
+	context "roll the dice method" do
+	  it "returns a number between 1 and 6" do
+	  	superhero = Superhero.new
+	  	a_number = superhero.roll_the_dice
+	  	expect(a_number).to be_between(1,6)
+	  end
+	end
 end
